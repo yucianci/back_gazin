165a247e268e74e4c149869d215e067c9e786e3b
Yuri Cianci <yucianci@gmail.com>
2022-12-22 01:45:50 -0300

    :rocket: Feat: Added configs to deploy on Heroku

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { NestFactory } from '@nestjs/core';
+import { env } from 'process';
 import { AppModule } from './app.module';
 const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
-  await app.listen(3333);
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
