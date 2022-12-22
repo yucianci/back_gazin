4892b5ce353a0b9f7b0a94ba03074cd9c731d4ab
Yuri Cianci <yucianci@gmail.com>
2022-12-22 09:28:41 -0300

    Testing deploy on AWS

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
   await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
